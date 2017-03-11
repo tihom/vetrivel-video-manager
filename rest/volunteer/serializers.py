12553ebd8a62641c81b09183607e3c6482928ef2
Mohit Aggarwal <mohitagg@gmail.com>
2017-03-11 00:33:20 -0800

    bug fixes in volunteer views after testign with front end

--- rest/volunteer/serializers.py
+++ rest/volunteer/serializers.py
@@ -3,19 +3,23 @@ from .models import Volunteer
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
-
+    name = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+    first_name = serializers.CharField(required=False, allow_blank=True)
+    last_name = serializers.CharField(required=False, allow_blank=True)
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name',)
+        fields = ('id', 'name', 'email', 'first_name', 'last_name',)
 
 class VolunteerSerializer(serializers.ModelSerializer):
     # field from the user model
-    username = serializers.CharField(source='user.username', required=False)
-    email = serializers.CharField(source='user.email', required=False)
-    first_name = serializers.CharField(source='user.first_name', required=False)
-    last_name = serializers.CharField(source='user.last_name', required=False)
+    id = serializers.CharField(source='user.id', required=False)
+    name = serializers.CharField(source='user.username', required=True)
+    email = serializers.CharField(source='user.email', required=True)
+    first_name = serializers.CharField(source='user.first_name', required=False, allow_blank=True)
+    last_name = serializers.CharField(source='user.last_name', required=False, allow_blank=True)
 
     class Meta:
         model = Volunteer
-        fields = ('user_id', 'username', 'email', 'first_name', 'last_name',
-            'phone_number', 'leader') 
+        fields = ('id', 'name', 'email', 'first_name', 'last_name',
+            'phone', 'leader') 
